96c0aec0db6c9961ea9b13b958389c59d59a8eb3
Esteban <esteban030991@gmail.com>
2022-10-09 17:27:03 -0300

    back-end Professores

---
+++ teacher/serializers.py
@@ -0,0 +1,26 @@
+from dataclasses import fields
+from django.forms import ValidationError
+from rest_framework import serializers
+
+from teacher.models import Professor, Aula
+
+class ProfessorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Professor
+        fields = '__all__'
+
+class CadastrarAulaSerializer(serializers.Serializer):
+    email = serializers.EmailField(max_length=255)
+    nome = serializers.CharField(max_length=100)
+    
+    def validate_nome(self, value):
+        if len(value) < 3:
+            raise ValidationError("deve ter pelo menos três caracteres")
+        return value
+    
+    
+    
+class AulaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Aula
+        fields = '__all__'
